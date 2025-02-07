de8dc968c27029fb2f339d45e7eb699ab24295bf
jbuchstaller <jbuchstaller@users.noreply.github.com>
2025-02-07 11:19:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.2",
     packages=find_packages(),
     entry_points= {
-        'group_1': 'run = geolocation.geolocation_api_call:main'
+        'group_1': 'run = geolocation.geolocation_api_call:main',
+        'console_scripts': ['custom_entry_point = geolocation.geolocation_api_call:main']
     },
 )
